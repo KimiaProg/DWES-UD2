26e126ce86b4070c6a51eeb01496b61cc2489f31
KimiaProg <kimia.ehsani.dolati.al@iespoligonosur.org>
2021-09-28 13:11:11 +0200

    ejercicio3

---
+++ ejercicio3.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $precioPantalon = 29.99;
+        $cantidadPantalon = 3;
+        $precioCamisa= 25.99;
+        $cantiadCamisa=2;
+        define("des", 0.08);
+
+        $total= ($precioCamisa*2) + ($precioPantalon*3);
+        $totalDes= $total-($total * des);
+
+        printf("El precio de un pantalón= %s <br>",$precioPantalon);
+        printf("El precio de una camisa= %s <br>",$precioCamisa);
+        printf("El total sin descuento es= %s <br>",$total);
+        printf("El total con descuento es= %s  <br>",$totalDes);
+
+    ?>
+</body>
+</html>
